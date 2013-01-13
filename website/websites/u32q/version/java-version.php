dea859acb2a8e6af2598ed170385513687ef1c1e
igouy-guest <igouy-guest>
2013-01-13 23:01:28 +0000

    Java 1.7.0_10

--- website/websites/u32q/version/java-version.php
+++ website/websites/u32q/version/java-version.php
@@ -1,4 +1,4 @@
-<p>java version "1.7.0_06"<br/>
-Java(TM) SE Runtime Environment (build 1.7.0_06-b24)<br/>
-Java HotSpot(TM) Server VM (build 23.2-b09, mixed mode)
+<p>java version "1.7.0_10"<br/>
+Java(TM) SE Runtime Environment (build 1.7.0_10-b18)<br/>
+Java HotSpot(TM) Server VM (build 23.6-b04, mixed mode)
 </p>
