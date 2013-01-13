367536fcc83a88565b3cce7e94a7b8324528a439
igouy-guest <igouy-guest>
2013-01-13 02:08:51 +0000

    Scala 2.10.0

--- website/websites/u32q/version/scala-version.php
+++ website/websites/u32q/version/scala-version.php
@@ -1,5 +1,5 @@
-<p>java version "1.7.0_06"<br/>
-Java(TM) SE Runtime Environment (build 1.7.0_06-b24)<br/>
-Java HotSpot(TM) Server VM (build 23.2-b09, mixed mode)
+<p>java version "1.7.0_10"<br/>
+Java(TM) SE Runtime Environment (build 1.7.0_10-b18)<br/>
+Java HotSpot(TM) Server VM (build 23.6-b04, mixed mode)
 </p>
-<p>Scala compiler version 2.9.2 -- Copyright 2002-2011, LAMP/EPFL</p>
+<p>Scala compiler version 2.10.0 -- Copyright 2002-2012, LAMP/EPFL</p>
