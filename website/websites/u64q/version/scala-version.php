e14281b32e40a8872fa5f8d6f9919d7fcff96695
igouy-guest <igouy-guest>
2013-01-13 05:25:37 +0000

    Scala 2.10.0

--- website/websites/u64q/version/scala-version.php
+++ website/websites/u64q/version/scala-version.php
@@ -2,4 +2,4 @@
 Java(TM) SE Runtime Environment (build 1.7.0_10-b18)<br/>
 Java HotSpot(TM) 64-Bit Server VM (build 23.6-b04, mixed mode)
 </p>
-<p>Scala compiler version 2.10.0-RC5 -- Copyright 2002-2012, LAMP/EPFL</p>
+<p>Scala compiler version 2.10.0 -- Copyright 2002-2012, LAMP/EPFL</p>
