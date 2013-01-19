b06d1b2c5f25bd5002072fc40caff6d34be8505a
igouy-guest <igouy-guest>
2013-01-19 18:48:23 +0000

    JRuby 1.7.2

--- website/websites/u64q/version/jruby-version.php
+++ website/websites/u64q/version/jruby-version.php
@@ -1,1 +1,1 @@
-<p>jruby 1.7.0 (1.9.3p203) 2012-10-22 ff1ebbe on Java HotSpot(TM) 64-Bit Server VM 1.7.0_09-b05 [linux-amd64]</p>
+<p>jruby 1.7.2 (1.9.3p327) 2013-01-04 302c706 on Java HotSpot(TM) 64-Bit Server VM 1.7.0_11-b21 [linux-amd64]</p>
