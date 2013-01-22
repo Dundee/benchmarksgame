5862e45facb97c461b0bd9a64b05203244244c5e
igouy-guest <igouy-guest>
2013-01-22 16:14:29 +0000

    *** empty log message ***

--- website/websites/u32/version/clojure-version.php
+++ website/websites/u32/version/clojure-version.php
@@ -1,5 +1,5 @@
-<p>java version "1.7.0_06"<br/>
-Java(TM) SE Runtime Environment (build 1.7.0_06-b24)<br/>
-Java HotSpot(TM) Server VM (build 23.2-b09, mixed mode)
+<p>java version "1.7.0_11"<br/>
+Java(TM) SE Runtime Environment (build 1.7.0_11-b21)<br/>
+Java HotSpot(TM) Server VM (build 23.6-b04, mixed mode)
 </p>
 <p>Clojure 1.4.0</p>
