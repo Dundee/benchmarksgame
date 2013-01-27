e4a2c7b6e2259eeb8c7fb9726e873e5dfc1cd6ed
igouy-guest <igouy-guest>
2013-01-27 18:48:29 +0000

    *** empty log message ***

--- website/about/python3-about.tpl.php
+++ website/about/python3-about.tpl.php
@@ -1,3 +1,3 @@
 <?=$Version;?>
 <p>"3.x is the newest branch of Python and the intended future of the language."</p>
-<p>Home Page: <a href="http://www.python.org/download/releases/3.2/">Python 3.2 was released on February 20th, 2011.</a></p>
+<p>Home Page: <a href="http://www.python.org/download/releases/3.3.0/">Python 3.3.0 was released on September 29th, 2012.</a></p>
