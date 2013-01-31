df7e00d10160d07b4392eaa243800767f9b63af3
igouy-guest <igouy-guest>
2013-01-31 01:11:57 +0000

    GHC 7.6.2

--- website/websites/u64q/version/ghc-version.php
+++ website/websites/u64q/version/ghc-version.php
@@ -1,2 +1,2 @@
-<p>llvm version 2.9 (Ubuntu 2.9+dfsg-3ubuntu4)</p>
-<p>The Glorious Glasgow Haskell Compilation System, version 7.6.1</p>
+<p>llvm version 3.1-2ubuntu1</p>
+<p>The Glorious Glasgow Haskell Compilation System, version 7.6.2</p>
