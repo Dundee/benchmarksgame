4057ae2127308905838712c92f593910fa1a6d5a
igouy-guest <igouy-guest>
2013-02-04 18:04:01 +0000

    Perl 5.16.2

--- website/websites/u32/version/perl-version.php
+++ website/websites/u32/version/perl-version.php
@@ -1,4 +1,4 @@
-<p>This is perl 5, version 14, subversion 1 (v5.14.1) built for i686-linux</p>
-<p>Compile-time options: PERL_DONT_CREATE_GVSV PERL_MALLOC_WRAP PERL_PRESERVE_IVUV USE_LARGE_FILES USE_PERLIO USE_PERL_ATOF</p>
+<p>This is perl 5, version 16, subversion 2 (v5.16.2) built for i686-linux</p>
+<p>Compile-time options: HAS_TIMES PERLIO_LAYERS PERL_DONT_CREATE_GVSV PERL_MALLOC_WRAP PERL_PRESERVE_IVUV USE_LARGE_FILES USE_LOCALE USE_LOCALE_COLLATE USE_LOCALE_CTYPE USE_LOCALE_NUMERIC USE_PERLIO USE_PERL_ATOF</p>
 <p>optimize='-O2'</p>
 <p>Compare to <a href="http://shootout.alioth.debian.org/u32q/perl.php">built for i686-linux-thread-multi</a></p>
