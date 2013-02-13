9ec72a44c33ccb0ec2b798a08fce3bb2f76f0b33
igouy-guest <igouy-guest>
2013-02-13 14:34:08 +0000

    *** empty log message ***

--- website/websites/u64/version/fsharp-version.php
+++ website/websites/u64/version/fsharp-version.php
@@ -1,3 +1,3 @@
 <p>Microsoft (R) F# 3.0 Compiler version (Mono build)</p>
-<p>Mono JIT compiler version 3.0.0 (tarball Sun Oct 21 19:53:36 PDT 2012)<br/>
+<p>Mono JIT compiler version 3.0.3 (tarball Tue Feb 12 21:22:20 PST 2013)<br/>
 	LLVM:          yes(3.2svn-mono)</p>
--- website/websites/u64q/version/csharp-version.php
+++ website/websites/u64q/version/csharp-version.php
@@ -1,2 +1,2 @@
-<p>Mono JIT compiler version 3.0.0 (tarball Sun Oct 21 19:53:36 PDT 2012)<br/>
+<p>Mono JIT compiler version 3.0.3 (tarball Tue Feb 12 21:22:20 PST 2013)<br/>
 	LLVM:          yes(3.2svn-mono)</p>
