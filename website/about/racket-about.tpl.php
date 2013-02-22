f319907eb1392aa1303776ffb4bb9839367cedb7
igouy-guest <igouy-guest>
2013-02-22 21:17:00 +0000

    *** empty log message ***

--- website/about/racket-about.tpl.php
+++ website/about/racket-about.tpl.php
@@ -1,3 +1,3 @@
 <?=$Version;?>
 <p>Home Page: <a href="http://www.racket-lang.org/">Racket</a></p>
-<p>Download: <a href="http://www.racket-lang.org/download/">Download Racket v5.3.1 (November 2012)</a></p>
+<p>Download: <a href="http://www.racket-lang.org/download/">Download Racket v5.3.3 (February 2013)</a></p>
