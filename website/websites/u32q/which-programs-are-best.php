7cceeb440803a618395458e7af595991e28ee043
igouy-guest <igouy-guest>
2013-03-04 16:19:47 +0000

    *** empty log message ***

--- website/websites/u32q/which-programs-are-best.php
+++ website/websites/u32q/which-programs-are-best.php
@@ -1,7 +1,6 @@
 <?php 
-// Copyright (c) Isaac Gouy 2012
+// Copyright (c) Isaac Gouy 2013
 ob_start('ob_gzhandler');
 require_once('config.php'); 
-$T = 'all'; $L = 'all';
-require_once(LIB_PATH.'scorecard.php'); 
+require_once(LIB_PATH.'boxplot.php');
 ?>
