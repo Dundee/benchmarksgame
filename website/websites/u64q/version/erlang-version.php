fb4fec42b1f7573c91128540bb9699526a565556
igouy-guest <igouy-guest>
2013-03-05 07:56:36 +0000

    Erlang R16B

--- website/websites/u64q/version/erlang-version.php
+++ website/websites/u64q/version/erlang-version.php
@@ -1,1 +1,1 @@
-<p>Erlang R16A (erts-5.10) [source] [64-bit] [smp:4:4] [async-threads:10] [kernel-poll:false]</p>
+<p>Erlang R16B (erts-5.10.1) [source] [64-bit] [smp:4:4] [async-threads:10] [kernel-poll:false]</p>
