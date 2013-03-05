9a42f34b72fa8bddafb93bb2a4a6b61e7d0876b5
igouy-guest <igouy-guest>
2013-03-05 20:52:08 +0000

    Erlang HiPE R16B

--- website/websites/u32q/version/hipe-version.php
+++ website/websites/u32q/version/hipe-version.php
@@ -1,1 +1,1 @@
-<p>Erlang R16A (erts-5.10) [source] [smp:4:4] [async-threads:10] [hipe] [kernel-poll:false]</p>
+<p>Erlang R16B (erts-5.10.1) [source] [smp:4:4] [async-threads:10] [hipe] [kernel-poll:false]</p>
