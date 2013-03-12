9d246dbc73b26043efda9ee14bd55d5506e6b541
igouy-guest <igouy-guest>
2013-03-12 19:06:00 +0000

    *** empty log message ***

--- website/about/boxplot-about.tpl.php
+++ website/about/boxplot-about.tpl.php
@@ -1,4 +1,4 @@
-<p>Please don't obsess about which programming language implementation is shown 10<sup>th</sup> and which is shown 11<sup>th</sup> -- the order would probably be different if it was based on the median scores instead of the geometric mean scores.</p>
+<p>Please don't obsess about which programming language implementation is shown 10<sup>th</sup> and which is shown 11<sup>th</sup> -- the order would probably be different if it was based on the median scores instead of <a href="http://portal.acm.org/citation.cfm?id=5666.5673" title="How not to lie with statistics: the correct way to summarize benchmark results">the geometric mean</a> scores.</p>
 
 <p>The <b>direct comparison</b> between 2 programming language implementations will give you a better understanding of the differences in program performance than this summary page.</p>
 <p>In some cases, a median score shown here is faster but the median time ratio shown in the direct comparison is slower. In those cases, the range of measurements for those 2 programming language implementations is really quite similar.</p>
