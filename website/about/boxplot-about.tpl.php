0fdcb8761e06ccc4a8b2361846e6c6490eac90b5
igouy-guest <igouy-guest>
2013-03-12 18:38:58 +0000

    *** empty log message ***

--- website/about/boxplot-about.tpl.php
+++ website/about/boxplot-about.tpl.php
@@ -1,3 +1,5 @@
+<p>Please don't obsess about which programming language implementation is shown 5<sup>th</sup> and which is shown 11<sup>th</sup> -- the order would probably be different if it was based on the median scores instead of the geometric mean scores.</p>
+
 <p>The <b>direct comparison</b> between 2 programming language implementations will give you a better understanding of the differences in program performance than this summary page.</p>
 <p>In some cases, a median score shown here is faster but the median time ratio shown in the direct comparison is slower. In those cases, the range of measurements for those 2 programming language implementations is really quite similar.</p>
 <p>(Remember, the scores shown here have been weighted with the time taken by the fastest program for each task, written in <i>any of the programming languages</i> -- the direct comparison is based solely on measurements for the 2 programming language implementations.)</p>
