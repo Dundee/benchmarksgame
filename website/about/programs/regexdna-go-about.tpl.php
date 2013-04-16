41ac23b4b2d4ea50f31d05af016cfebdca53c4d6
igouy-guest <igouy-guest>
2013-04-16 16:30:02 +0000

    *** empty log message ***

---
+++ website/about/programs/regexdna-go-about.tpl.php
@@ -0,0 +1,1 @@
+<p>See <a href="http://swtch.com/~rsc/regexp/regexp1.html">"Regular Expression Matching Can Be Simple And Fast (but is slow in Java, Perl, PHP, Python, Ruby, ...)"</a> Russ Cox, January 2007</p>
