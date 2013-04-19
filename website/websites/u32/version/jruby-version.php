d0bef0a47281b417c37c2b689acb7f5a9d5151ca
igouy-guest <igouy-guest>
2013-04-19 21:35:15 +0000

    JRuby 1.7.3

--- website/websites/u32/version/jruby-version.php
+++ website/websites/u32/version/jruby-version.php
@@ -1,1 +1,1 @@
-<p>jruby 1.7.2 (1.9.3p327) 2013-01-04 302c706 on Java HotSpot(TM) Server VM 1.7.0_11-b21 [linux-i386]</p>
+<p>jruby 1.7.3 (1.9.3p385) 2013-02-21 dac429b on Java HotSpot(TM) Server VM 1.7.0_11-b21 [linux-i386]</p>
