8624452fe6fd14fd7a22c7a2397adf640e023739
igouy-guest <igouy-guest>
2013-05-21 22:23:56 +0000

    Perl 5.18.0

--- website/websites/u32/version/perl-version.php
+++ website/websites/u32/version/perl-version.php
@@ -1,3 +1,6 @@
-<p>This is perl 5, version 16, subversion 2 (v5.16.2) built for i686-linux</p>
-<p>Compile-time options: HAS_TIMES PERLIO_LAYERS PERL_DONT_CREATE_GVSV PERL_MALLOC_WRAP PERL_PRESERVE_IVUV USE_LARGE_FILES USE_LOCALE USE_LOCALE_COLLATE USE_LOCALE_CTYPE USE_LOCALE_NUMERIC USE_PERLIO USE_PERL_ATOF</p>
-<p>optimize='-O2'</p>
+<p>This is perl 5, version 18, subversion 0 (v5.18.0) built for i686-linux</p>
+<pre>Compile-time options: HAS_TIMES PERLIO_LAYERS PERL_DONT_CREATE_GVSV
+                        PERL_HASH_FUNC_ONE_AT_A_TIME_HARD PERL_MALLOC_WRAP
+                        PERL_PRESERVE_IVUV PERL_SAWAMPERSAND USE_LARGE_FILES
+                        USE_LOCALE USE_LOCALE_COLLATE USE_LOCALE_CTYPE
+                        USE_LOCALE_NUMERIC USE_PERLIO USE_PERL_ATOF</pre>
