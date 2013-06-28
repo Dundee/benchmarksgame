46f6a19f099849d5874cf179866831f8849dae81
igouy-guest <igouy-guest>
2013-06-28 22:12:35 +0000

    F# 3.0 (Open Source Edition)

--- website/websites/u64q/version/fsharp-version.php
+++ website/websites/u64q/version/fsharp-version.php
@@ -1,3 +1,5 @@
-<p>Microsoft (R) F# 3.0 Compiler version (Mono build)</p>
-<p>Mono JIT compiler version 3.0.3 (tarball Tue Feb 12 21:22:20 PST 2013)<br/>
-	LLVM:          yes(3.2svn-mono)</p>
+<p>F# Compiler for F# 3.0 (Open Source Edition)</p>
+<p>Mono JIT compiler version 3.0.12 (tarball Thu Jun 20 11:46:48 PDT 2013)<br/>
+	LLVM:          yes(3.2svn-mono)<br/>
+	GC:            sgen
+</p>
