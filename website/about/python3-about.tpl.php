a46437b555c917e1e2505c77eb551ea483c5bef0
igouy-guest <igouy-guest>
2013-07-11 15:44:17 +0000

    *** empty log message ***

--- website/about/python3-about.tpl.php
+++ website/about/python3-about.tpl.php
@@ -1,3 +1,4 @@
 <?=$Version;?>
 <p>"3.x is the newest branch of Python and the intended future of the language."</p>
+<p><a href="http://www.infoworld.com/d/application-development/van-rossum-python-not-too-slow-188715">"Van Rossum: Python is not too slow"</a></p>
 <p>Home Page: <a href="http://www.python.org/download/releases/3.3.1/">Python 3.3.1 was released on April 7th, 2013.</a></p>
