56e9b21038dc2154df27c3e452d45c01f3b7656d
igouy-guest <igouy-guest>
2013-09-08 20:29:31 +0000

    *** empty log message ***

--- website/lib/play.tpl.php
+++ website/lib/play.tpl.php
@@ -47,7 +47,7 @@
 </dd>
 
 <dt><a href="#whymemory" name="whymemory"><strong>Why was memory was allocated?</strong></a></dt>
-<dd><p>Huge differences in <i>default</i> memory allocation, don't mean there'll be huge differences in memory use for tasks that require memory to be allocated:</p>
+<dd><p>Huge differences in <i>default</i> memory allocation, don't necessarily mean there'll be huge differences in memory use for tasks that require memory to be allocated:</p>
 <table>
 <tr>
 <th></th>
