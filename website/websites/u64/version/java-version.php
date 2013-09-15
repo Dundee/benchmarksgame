0583455fb3bf9300de7d6d92c87c25f01fd20fb8
igouy-guest <igouy-guest>
2013-09-15 06:55:57 +0000

    Java 1.7.0_40

--- website/websites/u64/version/java-version.php
+++ website/websites/u64/version/java-version.php
@@ -1,4 +1,4 @@
-<p>java version "1.7.0_11"<br/>
-Java(TM) SE Runtime Environment (build 1.7.0_11-b21)<br/>
-Java HotSpot(TM) 64-Bit Server VM (build 23.6-b04, mixed mode)
+<p>java version "1.7.0_40"<br/>
+Java(TM) SE Runtime Environment (build 1.7.0_40-b43)<br/>
+Java HotSpot(TM) 64-Bit Server VM (build 24.0-b56, mixed mode)
 </p>
