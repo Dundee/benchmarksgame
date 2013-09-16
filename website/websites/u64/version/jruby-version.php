263f0c3ef71688f7b1f7592d78315da8b990818d
igouy-guest <igouy-guest>
2013-09-16 22:32:39 +0000

    JRuby 1.7.4

--- website/websites/u64/version/jruby-version.php
+++ website/websites/u64/version/jruby-version.php
@@ -1,1 +1,1 @@
-<p>jruby 1.7.2 (1.9.3p327) 2013-01-04 302c706 on Java HotSpot(TM) 64-Bit Server VM 1.7.0_11-b21 [linux-amd64]</p>
+<p>jruby 1.7.4 (1.9.3p392) 2013-05-16 2390d3b on Java HotSpot(TM) 64-Bit Server VM 1.7.0_40-b43 +indy [linux-amd64]</p>
--- website/websites/u64q/version/jruby-version.php
+++ website/websites/u64q/version/jruby-version.php
@@ -1,1 +1,1 @@
-<p>jruby 1.7.3 (1.9.3p385) 2013-02-21 dac429b on Java HotSpot(TM) 64-Bit Server VM 1.7.0_11-b21 [linux-amd64]</p>
+<p>jruby 1.7.4 (1.9.3p392) 2013-05-16 2390d3b on Java HotSpot(TM) 64-Bit Server VM 1.7.0_40-b43 +indy [linux-amd64]</p>
