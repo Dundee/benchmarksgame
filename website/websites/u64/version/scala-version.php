104b311df2c815fef5bba74d947aee894830a265
igouy-guest <igouy-guest>
2013-09-17 20:38:13 +0000

    Scala 2.10.2

--- website/websites/u64/version/scala-version.php
+++ website/websites/u64/version/scala-version.php
@@ -1,5 +1,5 @@
-<p>java version "1.7.0_11"<br/>
-Java(TM) SE Runtime Environment (build 1.7.0_11-b21)<br/>
-Java HotSpot(TM) 64-Bit Server VM (build 23.6-b04, mixed mode)
+<p>java version "1.7.0_40"<br/>
+Java(TM) SE Runtime Environment (build 1.7.0_40-b43)<br/>
+Java HotSpot(TM) 64-Bit Server VM (build 24.0-b56, mixed mode)
 </p>
-<p>Scala compiler version 2.10.0 -- Copyright 2002-2012, LAMP/EPFL</p>
+<p>Scala compiler version 2.10.2 -- Copyright 2002-2013, LAMP/EPFL</p>
