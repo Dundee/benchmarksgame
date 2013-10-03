5999d9ec75372f3b9769d5dd3e1cc0141556c325
igouy-guest <igouy-guest>
2013-10-03 18:01:01 +0000

    *** empty log message ***

--- website/websites/soon-you-wont-find-the-benchmarks-game-with-google.php
+++ website/websites/soon-you-wont-find-the-benchmarks-game-with-google.php
@@ -2,6 +2,6 @@
 // Copyright (c) Isaac Gouy 2013
 ob_start('ob_gzhandler');
 require_once('config.php'); 
-$F = 'soon-you-wont-find-the-benchmarks-game-with-google'; $T = "Soon you won't find the Benchmarks Game with Google or any other search engine";
+$F = 'soon-you-wont-find-the-benchmarks-game-with-google'; $T = 'Soon you won't find the Benchmarks Game with Google or any other search engine';
 require_once(LIB_PATH.'miscfile.php');
 ?>
