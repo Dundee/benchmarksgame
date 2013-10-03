1ca96ad2aa255aae9bb1888d042ec5ef783deae5
igouy-guest <igouy-guest>
2013-10-03 19:05:40 +0000

    *** empty log message ***

--- website/misc/soon-you-wont-find-the-benchmarks-game-with-google.php
+++ website/misc/soon-you-wont-find-the-benchmarks-game-with-google.php
@@ -17,7 +17,7 @@
 
 <p>Not anymore!</p>
 
-<p>As a matter of policy, the Alioth admins have now <b>throttled web crawling</b> to reduce average server load from 30% to near nothing.</p>
+<p>As a matter of policy, the Alioth admins have now <b>throttled web crawling</b> to reduce average server load from 30% to near nothing. There's been no suggestion that the benchmarks game website was the primary cause of that server load. It just seems to be the result of providing dynamic content for 1,000 projects.</p>
 
 <p>The inevitable consequence is that all year Googlebot has struggled to crawl pages on the benchmarks game website. In mid-September Googlebot repeatedly failed to fetch robots.txt --</p>
 
@@ -37,10 +37,12 @@
 <p>-- and --</p>
 
 <pre>
-15 Sept "Time spent downloading a page (in milliseconds): 7042
+15 Sept Time spent downloading a page (in milliseconds): 7042
 25 Sept Time spent downloading a page (in milliseconds): 5460
 28 Sept Time spent downloading a page (in milliseconds): 6116
 </pre>
+
+<p>The inevitable consequence is that pages Googlebot cannot access are removed from Google's index. That's been happening all year. Since mid-September the benchmarks game has rapidly been removed from Google's index. Soon you won't find the Benchmarks Game website with Google or any other search engine, <b>because the Alioth admins have throttled web crawling</b>.</p>
 
 </dd>
 </dl>
