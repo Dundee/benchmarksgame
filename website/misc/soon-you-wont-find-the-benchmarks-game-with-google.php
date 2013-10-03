841c4af69a03eadb1a9f660b622e571f2e058b88
igouy-guest <igouy-guest>
2013-10-03 18:50:47 +0000

    *** empty log message ***

--- website/misc/soon-you-wont-find-the-benchmarks-game-with-google.php
+++ website/misc/soon-you-wont-find-the-benchmarks-game-with-google.php
@@ -13,11 +13,34 @@
 <dd>
 <p>Soon you won't find the Benchmarks Game with Google or any other search engine. So, if you're interested in programming language comparisons and performance benchmarks, <a href="http://benchmarksgame.alioth.debian.org/">bookmark the website now!</a></p>
 
-<p>Since 2004, The Computer Language Benchmarks Game has been hosted by <a href="http://lists.debian.org/debian-devel-announce/2003/03/msg00024.html">Alioth</a>. Mostly the <a href="https://wiki.debian.org/Alioth/Web">web hosting</a> has worked well enough for a low volume web site.</p>
+<p>Since 2004, The Computer Language Benchmarks Game has been hosted by <a href="http://lists.debian.org/debian-devel-announce/2003/03/msg00024.html">Alioth</a>, and the <a href="https://wiki.debian.org/Alioth/Web">web hosting</a> has mostly worked well enough for a low volume web site.</p>
 
 <p>Not anymore!</p>
 
-<p>As a matter of policy, the Alioth admins have now throttled web crawling to reduce average server load from 30% to near nothing.</p>
+<p>As a matter of policy, the Alioth admins have now <b>throttled web crawling</b> to reduce average server load from 30% to near nothing.</p>
+
+<p>The inevitable consequence is that all year Googlebot has struggled to crawl pages on the benchmarks game website. In mid-September Googlebot repeatedly failed to fetch robots.txt --</p>
+
+<pre>
+14 Sept errors/attempts 147 / 148
+15 Sept errors/attempts 39 / 39 
+24 Sept errors/attempts 80 / 81
+</pre>
+
+<p>-- and repeatedly failed to access the benchmarks game website --</p>
+
+<pre>
+16 Sept errors/attempts 266 / 271
+23 Sept errors/attempts 184 / 188
+</pre>
+
+<p>-- and --</p>
+
+<pre>
+15 Sept "Time spent downloading a page (in milliseconds): 7042
+25 Sept Time spent downloading a page (in milliseconds): 5460
+28 Sept Time spent downloading a page (in milliseconds): 6116
+</pre>
 
 </dd>
 </dl>
