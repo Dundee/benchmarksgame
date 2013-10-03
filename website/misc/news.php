65b9d9dbd26d56308ff49cf130b04d0caab744b5
igouy-guest <igouy-guest>
2013-10-03 16:16:52 +0000

    *** empty log message ***

--- website/misc/news.php
+++ website/misc/news.php
@@ -1,2 +1,27 @@
-<p>NEWS</p>
+<p><g:plusone annotation="none"></g:plusone></p>
+
+<script type="text/javascript">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+
+<dl>
+<dt><strong>NEWS</strong></dt>
+<dd>
+<dl>
+
+
+<dt><a href="#bookmark" name="bookmark">&nbsp;<strong>Bookmark the benchmarks game because Google won't find it anymore!</strong></a></dt>
+
+<dd>
+<dd>
+
+
+
+</dl>
+</dd>
+</dl>
 
--- website/websites/news.php
+++ website/websites/news.php
@@ -3,5 +3,5 @@
 ob_start('ob_gzhandler');
 require_once('config.php'); 
 $F = 'news'; $T = 'News';
-require_once(LIB_PATH.'news.php');
+require_once(LIB_PATH.'miscfile.php');
 ?>
