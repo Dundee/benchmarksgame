a8ff05baa1b4aa8dcf16c0581ea3bdbcc0496ab2
igouy-guest <igouy-guest>
2013-10-03 18:32:44 +0000

    *** empty log message ***

--- website/misc/soon-you-wont-find-the-benchmarks-game-with-google.php
+++ website/misc/soon-you-wont-find-the-benchmarks-game-with-google.php
@@ -11,13 +11,13 @@
 <dl>
 
 <dd>
-<p>Soon you won't find the Benchmarks Game with Google or any other search engine. So, if you're interested in programming language comparisons and performance benchmarks, <a href="http://benchmarksgame.alioth.debian.org/">bookmark the website now!</a>.</p>
+<p>Soon you won't find the Benchmarks Game with Google or any other search engine. So, if you're interested in programming language comparisons and performance benchmarks, <a href="http://benchmarksgame.alioth.debian.org/">bookmark the website now!</a></p>
 
 <p>Since 2004, The Computer Language Benchmarks Game has been hosted by <a href="http://lists.debian.org/debian-devel-announce/2003/03/msg00024.html">Alioth</a>. Mostly the <a href="https://wiki.debian.org/Alioth/Web">web hosting</a> has worked well enough for a low volume web site.</p>
 
 <p>Not anymore!</p>
 
-<p></p>
+<p>As a matter of policy, the Alioth admins have now throttled web crawling to reduce average server load from 30% to near nothing.</p>
 
 </dd>
 </dl>
