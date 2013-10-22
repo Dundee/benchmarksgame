86d53ea8be332be793435eec16dc13f6f5d2f320
igouy-guest <igouy-guest>
2013-10-22 21:59:34 +0000

    *** empty log message ***

--- website/about/fasta-about.tpl.php
+++ website/about/fasta-about.tpl.php
@@ -28,6 +28,6 @@ Random (Max)
 <li>write 3 sequences line-by-line in <a href="http://en.wikipedia.org/wiki/Fasta_format">FASTA format</a></li>
 </ul>
 
-<p>We'll use the generated FASTA file as input for other benchmarks (<a href="benchmark.php?test=revcomp&amp;lang=all&amp;sort=<?=$Sort;?>">reverse-complement</a>, <a href="benchmark.php?test=knucleotide&amp;lang=all&amp;sort=<?=$Sort;?>">k-nucleotide</a>).</p>
+<p>We'll use the generated FASTA file as input for other benchmarks (<a href="benchmark.php?test=revcomp&amp;lang=all">reverse-complement</a>, <a href="benchmark.php?test=knucleotide&amp;lang=all">k-nucleotide</a>).</p>
 
 <p>Random DNA sequences can be based on a variety of <a href="http://www.scmbb.ulb.ac.be/pub/jvanheld/courses/regulatory_sequence_analysis/pdf_files/random_models.pdf">Random Models</a> (554KB pdf). You can use Markov chains or independently distributed nucleotides to <a href="http://rsat.scmbb.ulb.ac.be/rsat/random-seq_form.cgi">generate random DNA sequences online</a>.</p>
