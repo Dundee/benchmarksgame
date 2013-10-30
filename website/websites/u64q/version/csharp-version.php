d0a7cc40321303ad184c9495b434cf758ebe8be5
igouy-guest <igouy-guest>
2013-10-30 00:48:02 +0000

    Mono 3.2.5

--- website/websites/u64q/version/csharp-version.php
+++ website/websites/u64q/version/csharp-version.php
@@ -1,4 +1,4 @@
-<p>Mono JIT compiler version 3.0.12 (tarball Thu Jun 20 11:46:48 PDT 2013)<br/>
-	LLVM:          yes(3.2svn-mono)<br/>
+<p>Mono Runtime Engine version 3.2.5 (master/4c1198f Tue Oct 29 09:53:35 PDT 2013)<br/>
+	LLVM:          yes(3.3svn-mono-mono/8bdedb5)<br/>
 	GC:            sgen
 </p>
--- website/websites/u64q/version/fsharp-version.php
+++ website/websites/u64q/version/fsharp-version.php
@@ -1,5 +1,5 @@
 <p>F# Compiler for F# 3.0 (Open Source Edition)</p>
-<p>Mono JIT compiler version 3.0.12 (tarball Thu Jun 20 11:46:48 PDT 2013)<br/>
-	LLVM:          yes(3.2svn-mono)<br/>
+<p>Mono Runtime Engine version 3.2.5 (master/4c1198f Tue Oct 29 09:53:35 PDT 2013)<br/>
+	LLVM:          yes(3.3svn-mono-mono/8bdedb5)<br/>
 	GC:            sgen
 </p>
