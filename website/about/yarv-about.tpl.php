723798d371101f18b4ed7a3d0b9d15721a79b938
igouy-guest <igouy-guest>
2014-01-09 16:39:10 +0000

    *** empty log message ***

--- website/about/yarv-about.tpl.php
+++ website/about/yarv-about.tpl.php
@@ -1,4 +1,4 @@
 <p>online book <a href="http://www.rubycentral.com/book/">Programming Ruby: The Pragmatic Programmers Guide</a></p>
 <?=$Version;?>
 <p>Home Page: <a href="http://www.ruby-lang.org/en/">Ruby is…</a></p>
-<p>Download: <a href="http://www.ruby-lang.org/en/news/2013/06/27/ruby-2-0-0-p247-is-released/">Ruby 2.0.0-p247 is released</a></p>
+<p>Download: <a href="http://www.ruby-lang.org/en/news/2013/12/25/ruby-2-1-0-is-released/">Ruby 2.1.0 is released</a></p>
