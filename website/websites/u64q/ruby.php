89d751fe7787786d361622e59269b2abff4f70bd
Daniel Milde <daniel@milde.cz>
2015-02-12 13:06:28 +0100

    test ranges, fixes

--- website/websites/u64q/ruby.php
+++ website/websites/u64q/ruby.php
@@ -1,7 +1,7 @@
 <?php
 // Copyright (c) Isaac Gouy 2009-2011
 ob_start('ob_gzhandler');
-require_once('config.php'); 
-$T = 'all'; $L = 'yarv'; $metaRobots = '';
-require_once(LIB_PATH.'compare.php'); 
+require_once('config.php');
+$T = 'all'; $L = 'ruby'; $metaRobots = '';
+require_once(LIB_PATH.'compare.php');
 ?>
