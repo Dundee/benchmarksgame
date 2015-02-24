6112ea2ff82ba186bbe5d243ad409cc9fd299513
Daniel Milde <daniel@milde.cz>
2015-02-24 18:59:06 +0100

    shedskin name fixed

--- website/websites/u64q/shedskin.php
+++ website/websites/u64q/shedskin.php
@@ -2,6 +2,6 @@
 // Copyright (c) Isaac Gouy 2009-2010
 ob_start('ob_gzhandler');
 require_once('config.php');
-$T = 'all'; $L = 'schedskin'; $metaRobots = '';
+$T = 'all'; $L = 'shedskin'; $metaRobots = '';
 require_once(LIB_PATH.'compare.php');
 ?>
