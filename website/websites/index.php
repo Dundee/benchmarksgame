ca1d412157fd82d799df144adacc1fb1fbc88e42
Daniel Milde <daniel@milde.cz>
2015-08-17 17:05:36 +0200

    nuitka updated

--- website/websites/index.php
+++ website/websites/index.php
@@ -75,7 +75,7 @@ $langs = array(
    array('pypy3','PyPy3 2.4.0','pypy3',$allsites),
    array('jython','Jython 2.7.0','jython',$allsites),
    array('cython','Cython 0.23','cython',$allsites),
-   array('nuitka','Nuitka 0.5.13.1','nuitka',$allsites),
+   array('nuitka','Nuitka 0.5.13.6','nuitka',$allsites),
    array('shedskin','Shedskin 0.9.4','shedskin',$allsites),
    array('numba','Numba 0.19.1','numba',$allsites),
    array('pyston','Pyston v0.3','pyston',$allsites),
