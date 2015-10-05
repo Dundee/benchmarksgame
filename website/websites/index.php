55bad41c7e18cab6adba45cdc6786299f08cf64e
Daniel Milde <daniel@milde.cz>
2015-10-05 12:35:13 +0200

    cython updated

--- website/websites/index.php
+++ website/websites/index.php
@@ -73,7 +73,7 @@ $langs = array(
    array('python36','Python 3.6.0a0','python36',$allsites),
    array('pypy3','PyPy3 2.4.0','pypy3',$allsites),
    array('jython','Jython 2.7.0','jython',$allsites),
-   array('cython','Cython 0.23.2','cython',$allsites),
+   array('cython','Cython 0.23.3','cython',$allsites),
    array('nuitka','Nuitka 0.5.14.3','nuitka',$allsites),
    array('shedskin','Shedskin 0.9.4','shedskin',$allsites),
    array('numba','Numba 0.19.1','numba',$allsites),
