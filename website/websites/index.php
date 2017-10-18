9d1178c4265d018ba971a193d4e0001ecee0b296
Daniel Milde <daniel.milde@firma.seznam.cz>
2017-10-18 11:33:48 +0200

    pypy 5.9.0, cython 0.27.1

--- website/websites/index.php
+++ website/websites/index.php
@@ -68,13 +68,13 @@ $allsites = array('u64q');
 
 $langs = array(
    array('python','Python 2.7.13 (with NumPy and gmpy)','python',$allsites),
-   array('pypy','PyPy 5.8.0 (with gmpy_cffi)','pypy',$allsites),
+   array('pypy','PyPy 5.9.0 (with gmpy_cffi)','pypy',$allsites),
    array('python3','Python 3.6.2 (with NumPy and gmpy2)','python3',$allsites),
    array('python-dev','Python 3.7.0a0','python-dev',$allsites),
-   array('pypy3','PyPy3 5.8.0 (with gmpy_cffi)','pypy3',$allsites),
+   array('pypy3','PyPy3 5.9.0 (with gmpy_cffi)','pypy3',$allsites),
    array('ipy','IronPython 2.7.8a1 (with Mono 5.0.0.100)','ipy',$allsites),
    array('jython','Jython 2.7.1 (with JDK 8.u131)','jython',$allsites),
-   array('cython','Cython 0.26.1 (with NumPy and gmpy)','cython',$allsites),
+   array('cython','Cython 0.27.1 (with NumPy and gmpy)','cython',$allsites),
    array('nuitka','Nuitka 0.5.27.0','nuitka',$allsites),
    array('shedskin','Shedskin 0.9.4','shedskin',$allsites),
    array('numba','Numba 0.34.0 (with NumPy and Anaconda 4.4.0)','numba',$allsites),
