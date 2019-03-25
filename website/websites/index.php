a293f64358f38a9ad064b3165d17ff58a8e08019
Daniel Milde <daniel.milde@firma.seznam.cz>
2019-03-25 14:02:43 +0100

    pypy, cython and graal updated

--- website/websites/index.php
+++ website/websites/index.php
@@ -68,20 +68,20 @@ $allsites = array('u64q');
 
 $langs = array(
    array('python','Python 2.7.15 (with NumPy and gmpy)','python',$allsites),
-   array('pypy','PyPy 7.0.0 (with gmpy_cffi)','pypy',$allsites),
+   array('pypy','PyPy 7.1.0 (with gmpy_cffi)','pypy',$allsites),
    array('python3','Python 3.7.1 (with NumPy and gmpy2)','python3',$allsites),
    array('python-dev','Python 3.8.0a0.r102209','python-dev',$allsites),
-   array('pypy3','PyPy3 7.0.0 (with gmpy_cffi)','pypy3',$allsites),
+   array('pypy3','PyPy3 7.1.0 (with gmpy_cffi)','pypy3',$allsites),
    array('ipy','IronPython 2.7.8a1 (with Mono 5.0.0.100)','ipy',$allsites),
    array('jython','Jython 2.7.1 (with JDK 8.u131)','jython',$allsites),
-   array('cython','Cython 0.29.5 (with NumPy and gmpy)','cython',$allsites),
+   array('cython','Cython 0.29.6 (with NumPy and gmpy)','cython',$allsites),
    array('nuitka','Nuitka 0.6.2','nuitka',$allsites),
    array('shedskin','Shedskin 0.9.4','shedskin',$allsites),
    array('numba','Numba 0.35.0 (with NumPy and Anaconda 5.0.1)','numba',$allsites),
    array('pyston','Pyston 0.6.1','pyston',$allsites),
    array('micropython','MicroPython 1.10','micropython',$allsites),
    array('grumpy','Grumpy r123.08f9c2e','grumpy',$allsites),
-   array('graal','Graal python 1.0.0_rc12','graal',$allsites),
+   array('graal','Graal python 1.0.0_rc14','graal',$allsites),
    );
 
 
